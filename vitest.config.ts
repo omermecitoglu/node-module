f17516a2709b669c7b14b96c562f7a79ecdc6c17
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-07-04 13:36:30 +0700

    chore: update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,14 @@ import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  test: {
+    coverage: {
+      include: ["src/**/*.ts"],
+      exclude: ["src/index.ts"],
+    },
+  },
+  plugins: [
+  ],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
